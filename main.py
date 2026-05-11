7dbbb5ebecaf3800daea6eaf286ad115d540945d
GoDotProg <aimed2012ija@gmail.com>
2026-05-11 21:16:56 +0300

    Implement array creation and value insertion functions
    Added functions to create and insert values into an array.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 Array = []
 
+def create_array():
+    global Array
+    Array = [0] * 10
+
+def insert_value(index, value):
+    if index < len(Array):
+        Array[index] = value
+    else:
+        print("Index out of bounds")
+    
+print("Creating array...")
+create_array()
+print("Inserting values...")
+insert_value(0, 5)
+insert_value(1, 10)
